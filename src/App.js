9a529330d4b2f7d34346bb3711dcb3a0f047ab98
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-01-12 14:18:39 +0530

    modify the user input validity with useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,11 +7,20 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => { //whenever you have an action that should execute in response to some other action that is a side effect
+    const storedUserLoggedInformation = localStorage.getItem('isLoggedIn')
+    if(storedUserLoggedInformation === '1') {
+      setIsLoggedIn(true)
+    }
+  }, []) //runs only when the app starts, becz dependencies haven't changed
+
   const loginHandler = (email, password) => {
+    localStorage.setItem('isLoggedIn', '1')
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn')
     setIsLoggedIn(false);
   };
 
